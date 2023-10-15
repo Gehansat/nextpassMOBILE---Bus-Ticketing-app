dd7d0ad1b264514b9f7b5d81c4bc288f2b4a39ac
IT21108990 <IT21108990@my.sliit.lk>
2023-10-15 19:01:39 +0530

    change main file

---
+++ lib/pages/LoginOption.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Loginoption extends StatelessWidget {
+  const Loginoption({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Kaveen'),
+      ),
+    );
+  }
+}
