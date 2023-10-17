af87af1a84e15091414834963db19926b2d05515
IT21108990 <IT21108990@my.sliit.lk>
2023-10-17 15:17:43 +0530

    splashscreen, login option
    added poppins

--- lib/pages/LoginOption.dart
+++ lib/pages/LoginOption.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Loginoption extends StatelessWidget {
@@ -5,9 +6,54 @@ class Loginoption extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Kaveen'),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Explore, Connect,\nTravel Together!",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 50),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Color(0xff0048FF),
+                onPrimary: Colors.white,
+                fixedSize: Size(245, 60),
+                textStyle: const TextStyle(
+                  fontSize: 20
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(context).push(
+                  CupertinoPageRoute(builder: (ctx) => Loginoption()),
+                );
+
+              },
+              child: Text('Local Passenger'),
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Color(0xff0048FF),
+                onPrimary: Colors.white,
+                fixedSize: Size(245, 60),
+                textStyle: const TextStyle(
+                    fontSize: 20
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(context).push(
+                  CupertinoPageRoute(builder: (ctx) => Loginoption()),
+                );
+              },
+              child: Text('Foreginer Passenger'),
+            ),
+          ],
+        )
       ),
     );
   }
